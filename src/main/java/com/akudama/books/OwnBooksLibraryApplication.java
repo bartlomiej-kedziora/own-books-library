cafbb7ac1596c98dbcdd2fea9c77552097888675
Bartlomiej Kedziora <bartlomiej.kedziora@vp.pl>
2019-01-02 00:54:53 +0100

    init project

---
+++ src/main/java/com/akudama/books/OwnBooksLibraryApplication.java
@@ -0,0 +1,14 @@
+package com.akudama.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnBooksLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwnBooksLibraryApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/akudama/books/OwnBooksLibraryApplicationTests.java
@@ -0,0 +1,17 @@
+package com.akudama.books;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OwnBooksLibraryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
