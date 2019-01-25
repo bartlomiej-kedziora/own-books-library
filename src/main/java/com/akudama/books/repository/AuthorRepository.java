44e5a90d3bb006d9f2254567bf34e70563bd15dc
Bartlomiej Kedziora <bartlomiej.kedziora@vp.pl>
2019-01-26 00:45:30 +0100

    add repositories

---
+++ src/main/java/com/akudama/books/repository/AuthorRepository.java
@@ -0,0 +1,27 @@
+package com.akudama.books.repository;
+
+import com.akudama.books.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+    @Override
+    List<Author> findAll();
+
+    Optional<Author> findById();
+
+    @Override
+    Author save(Author author);
+
+    @Override
+    void delete(Author author);
+
+    void deleteById(long id);
+
+    @Override
+    long count();
+}
